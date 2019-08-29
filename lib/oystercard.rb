bb8f11a2411f1996ea4b1f895637b0a62145b311
Valeria Serci <balerty84@gmail.com>
2019-08-29 15:08:40 +0100

    Refactor: rspec and context; implement deduct method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,19 +1,21 @@
 class Oystercard
   attr_reader :balance
   LIMIT = 90
-
+ERROR_LIMIT_EXCEEDED = "top up limit of #{LIMIT} reached"
   def initialize
     @balance =  0
   end
 
   def top_up(amount)
 
-    raise "top up limit of #{LIMIT} reached" if  (@balance + amount) > 90
+    raise ERROR_LIMIT_EXCEEDED if (@balance + amount) > 90
 
     @balance += amount
   end
 
-
+def deduct(amount)
+  @balance -= amount
+end
 
 
 end
--- spec/osytercard_spec.rb
+++ spec/osytercard_spec.rb
@@ -2,23 +2,31 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it { is_expected.to respond_to :top_up }
-
-  it "has a balance of zero" do
-    expect(subject.balance).to eq(0)
+  context '#initialize a new card' do
+    it { is_expected.to respond_to :top_up }
+    it { is_expected.to respond_to :deduct }
   end
 
-  it 'increases the balance' do
-    expect { subject.top_up(1) }.to change {subject.balance }.from(0).to(subject.balance+1)
+  context '#top_up' do
+    it "has a balance of zero" do
+      expect(subject.balance).to eq(0)
+    end
+
+    it 'increases the balance' do
+      expect { subject.top_up(1) }.to change {subject.balance }.from(0).to(subject.balance+1)
+    end
+
+    it 'raises error when the limit is exceeded' do
+      expect { subject.top_up(91) }.to raise_error (Oystercard::ERROR_LIMIT_EXCEEDED)
+    end
   end
 
-  it 'raises error when the limit is exceeded' do
-
-     # subject.top_up(91)
-    expect { subject.top_up(91) }.to raise_error ("top up limit of #{Oystercard::LIMIT} reached")
+  context '#deduct' do
+    it 'deducts money' do
+      subject.top_up(90)
+      amount = 90
+    expect { subject.deduct(90) }.to change {subject.balance }.by -amount
+    end
   end
-
-  it 'deducts money'
-  expect { subject.deduct(90) }.to change {subject.balance }.from(90).to(subject.balance-1)
 
 end
